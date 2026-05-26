2babab2afed2f3f3716275ae66a41795e327805e
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-05-26 07:55:23 +0300

    removed redundant aud

--- src/deps.rs
+++ src/deps.rs
@@ -10,14 +10,15 @@ where
 {
     async fn async_from(ctx: &EnvContext) -> Result<Self, EnvError> {
         let signer_type = ctx.get("validate.type")?;
+        let aud = ctx.get("validate.aud")?.to_string();
         match signer_type {
             "hs256" => Ok(OrphanWrapper(Arc::new(HS256Signer::new(
-                ctx.get("validate.aud")?.to_owned(),
+                aud,
                 ctx.get("validate.secret")?.to_owned(),
             )) as Arc<dyn Validate<T>>)),
             "rs256" => Ok(OrphanWrapper(Arc::new(RS256Validator::new(
                 ctx.get("validate.public_key")?.to_string(),
-                ctx.get("validate.aud")?.to_string(),
+                aud,
             )) as Arc<dyn Validate<T>>)),
             _ => Err(EnvError::new(format!(
                 "Unsupported validate.type value: {signer_type}"
@@ -36,7 +37,7 @@ where
         match signer_type {
             "hs256" => Ok(OrphanWrapper(Arc::new(HS256Signer::new(
                 aud,
-                ctx.get("validate.secret")?.to_owned(),
+                ctx.get("sign.secret")?.to_owned(),
             )) as Arc<dyn Sign<T>>)),
             "rs256" => Ok(OrphanWrapper(Arc::new(RS256Signer::new(
                 ctx.get("sign.private_key")?.to_string(),
